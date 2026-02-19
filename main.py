4567e9584ac499dcc93c8f359837a8001290fc32
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2026-02-19 10:26:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -181,8 +181,8 @@ if uploaded_file:
         ax1.set_xlabel("X")
         ax1.set_ylabel("Y")
         #ax1.set_aspect("equal", adjustable="datalim")
-        ax1.xlim(0,1440)
-        ax1.ylim(0,2730)
+        ax1.set_xlim(0,1440)
+        ax1.set_ylim(0,2730)
         ax1.legend()
         st.pyplot(fig1)
 
